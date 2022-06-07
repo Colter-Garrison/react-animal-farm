b5882da2e186d2fb958d0caeca3ea6ebd2d58a24
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-07 14:24:56 -0700

    footer working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './Header/Header.js';
-import Main from './Main/Main.js';
+// import Main from './Main/Main.js';
 import Footer from './Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header className="App-header" name='Colter' />
-      <main />
-      <footer />
+      {/* <Main /> */}
+      <Footer year='2022' />
     </div>
   );
 }
